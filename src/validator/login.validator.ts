515468742e55b1870a8109f006c42a875d6162a6
YoussefRashad <youssefrashad119@gmail.com>
2022-03-05 23:35:25 +0200

    add options for joi validate

--- src/validator/login.validator.ts
+++ src/validator/login.validator.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const loginValidator = Joi.object({
-  identifier: Joi.string().required(),
+  email: Joi.string().email().required(),
   password: Joi.string()
     .regex(/[a-zA-Z0-9]{6,30}/)
     .required(),
--- src/validator/validate.ts
+++ src/validator/validate.ts
@@ -4,7 +4,12 @@ import Request from "../types/Request";
 
 export default (validator: Joi.ObjectSchema<any>) =>
   async function (req: Request, res: Response, next: NextFunction) {
-    const validated = await validator.validateAsync(req.body);
+    const options = {
+      abortEarly: false, // include all errors
+      allowUnknown: true, // ignore unknown props
+      stripUnknown: true, // remove unknown props
+    };
+    const validated = await validator.validateAsync(req.body, options);
     req.body = validated;
     next();
   };
