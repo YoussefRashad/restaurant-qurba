8d17af49abdcb2c6554dd8ea6003c3770d6c778c
YoussefRashad <youssefrashad119@gmail.com>
2022-03-05 22:12:55 +0200

    add user routers

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,25 @@
+import User, { FindByCredentials } from "../models/user.model";
+import Request from "../types/Request";
+import { Response } from "express";
+import HttpStatusCodes from "http-status-codes";
+
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const user = new User({ ...req.body });
+    const token = await user.generateAuthToken();
+    res.status(HttpStatusCodes.CREATED).send({ user, token });
+  } catch (error) {
+    res.status(HttpStatusCodes.BAD_GATEWAY).send({ error: error.message });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await FindByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.status(HttpStatusCodes.ACCEPTED).send({ user, token });
+  } catch (error) {
+    res.status(HttpStatusCodes.BAD_GATEWAY).send({ error: error.message });
+  }
+};
--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -1,8 +1,10 @@
-import { Router, Response }  from 'express'
+import { Router }  from 'express'
+import { login, signup } from '../../controller/user.controller'
 
 const router: Router = Router()
 
-
+router.post('/signup', signup)
+router.post('/login', login)
 
 
 export default router;
